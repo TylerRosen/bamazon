8bc386eda91c82f7793aa888f77496ce88f64748
TylerRosen <tylermrosen@gmail.com>
2017-02-23 14:38:20 -0500

    Fixed issues, works properly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,6 +21,14 @@ connection.query('SELECT * from products', function (error, results, fields) {
 
     console.log("\n");
 
+});
+
+purchase();
+
+
+
+var purchase = function () {
+
     inquirer.prompt([{
         type: "input",
         name: "product",
@@ -29,47 +37,59 @@ connection.query('SELECT * from products', function (error, results, fields) {
 
         var product = data.product;
 
-        connection.query('SELECT * from products', function (error, results, fields) {
+        connection.query('SELECT * from products WHERE product_name = ?', [product], function (error, results, fields) {
 
             inquirer.prompt([{
                 type: "input",
                 name: "quantity",
                 message: "Select quantity"
             }]).then(function (data) {
-                
-        //         if (product.stock_quantity < data.quantity) {
-        //             console.log("Insufficient quantity!")
-        //         } else {
-        //     connection.query("UPDATE products SET ? WHERE ?", [{
-        //         stock_quantity: product.stock_quantity - data.quantity
-        //     }, {
-        //         id: product.id
-        //     }], function(err, res) {
-        //         if (err) throw err;
-        //         console.log("PRODUCTS TABLE UPDATED");
-        //     });
 
-        //     connection.query("INSERT INTO " + table + " SET ?", {
-        //         product_id: data.purchaseProductId,
-        //         quantity_purchased: data.quantity,
-        //         created_at: NOW()
-        //     }, function(err, res) {
-        //         console.log('SALES TABLE UPDATED!')
-        //     });
-        // }
+                console.log(data.quantity);
+
+                console.log("PRODUCT STOCK QUANTITY   " + results[0].stock_quantity);
+
+                if (results[0].stock_quantity < parseInt(data.quantity)) {
+                    console.log("Insufficient quantity!")
+                    connection.end();
+
+                } else {
+
+                    console.log('product id  ' + results[0].id);
+
+
+                    var difference = results[0].stock_quantity - (parseInt(data.quantity));
+
+                    console.log("DIFFERENCE" + difference);
+                    console.log("DATA PRODUCT " + results[0].product_name)
+                    
+                    connection.query("UPDATE products SET stock_quantity = ? WHERE product_name = ?", [difference, results[0].product_name], function (err, res) {
+                        if (err) console.log('error: ' + err);
+                        console.log("PRODUCTS TABLE UPDATED");
+
+                        // return(res);
+
+                        console.log("DATA PURCHASE  " + results[0].id);
+                        // console.log(NOW());
+                        connection.query("INSERT INTO sales SET ?", {
+                            product_id: results[0].id,
+                            quantity_purchased: parseInt(data.quantity),
+                            created_at: "NOW()"
+                        }, function (err, res) {
+                            console.log('SALES TABLE UPDATED!')
+                            connection.end();
+                        });
+                    });
+
+
+                }
 
 
             });
 
         });
 
-        // if (item.stock_quantity > quantity) {
-        //     console.log("Insufficient quantity!")
-
-        // 
 
     });
 
-});
-
-connection.end();
+};
